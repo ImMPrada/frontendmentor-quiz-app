e29e048d6eb5a004016da11db9ec5c1bd2690ad7
immprada <im.mprada@gmail.com>
2025-03-16 09:32:31 -0500

    refactor: actualiza componente Icon con contenedor para los iconos

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -2,18 +2,17 @@ import { IconProps } from './types';
 import { iconsNames } from './icons-names';
 import invariant from 'tiny-invariant';
 
-export default function Icon({ name, className, color, width = '24', height = '24' }: IconProps) {
+export default function Icon({ name, className, color, width = '24px', height = '24px' }: IconProps) {
   const src = iconsNames[name];
   invariant(src, `Icon ${name} not found`);
   
   return (
-    <img 
-      src={iconsNames[name]} 
-      className={className}
-      width={width}
-      height={height}
-      alt={`${name} icon`}
-      style={{ filter: `brightness(0) saturate(100%) ${color ? `fill(${color})` : ''}` }}
-    />
+    <div className="min-w-10 min-h-10 border border-red p-1 rounded-lg">
+      <img
+        src={iconsNames[name]} 
+        alt={`${name} icon`}
+        // style={{ filter: `brightness(0) saturate(100%) ${color ? `fill(${color})` : ''}` }}
+      />
+    </div>
   )
 }
